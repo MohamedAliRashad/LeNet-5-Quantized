4a72d5f644673ac41d4853c1202105047614fb83
MohamedAliRashad <m.rashadnow@gmail.com>
2019-06-22 11:20:21 +0200

    initial Add

---
+++ Keras_LeNet.py
@@ -0,0 +1,94 @@
+import keras
+from keras import layers, models
+from keras.datasets import mnist
+from keras.models import Sequential
+from keras.utils import np_utils
+import numpy as np
+
+def LeNet():
+        
+    #Instantiate an empty model
+    model = Sequential()
+
+    # C1 Convolutional Layer
+    model.add(layers.Conv2D(6, kernel_size=(5, 5), strides=(1, 1), activation="tanh", input_shape=(28,28,1), padding="same"))
+
+    # S2 Pooling Layer
+    model.add(layers.AveragePooling2D(pool_size=(2, 2), strides=(1, 1), padding="valid"))
+
+    # C3 Convolutional Layer
+    model.add(layers.Conv2D(16, kernel_size=(5, 5), strides=(1, 1), activation="tanh", padding="valid"))
+
+    # S4 Pooling Layer
+    model.add(layers.AveragePooling2D(pool_size=(2, 2), strides=(2, 2), padding="valid"))
+
+    # C5 Fully Connected Convolutional Layer
+    model.add(layers.Conv2D(120, kernel_size=(5, 5), strides=(1, 1), activation="tanh", padding="valid"))
+    #Flatten the CNN output so that we can connect it with fully connected layers
+    model.add(layers.Flatten())
+
+    # FC6 Fully Connected Layer
+    model.add(layers.Dense(84, activation="tanh"))
+
+    #Output Layer with softmax activation
+    model.add(layers.Dense(10, activation="softmax"))
+
+    # Compile the model
+    model.compile(loss=keras.losses.categorical_crossentropy, optimizer="SGD", metrics=["accuracy"])
+
+    return model
+
+def Train(model, output_path, x_train, y_train):
+
+    model.fit(x=x_train,y=y_train, epochs=10, batch_size=128, validation_data=(x_test, y_test), verbose=1)
+    model.save(output_path)
+
+def Test(model, weights_path, x_test, y_test):
+    
+    # Load Weights
+    model.load_weights(weights_path)
+    
+    test_score = model.evaluate(x_test, y_test)
+    print("Test loss {:.4f}, accuracy {:.2f}%".format(test_score[0], test_score[1] * 100))
+
+
+def print_weights(model, weights_path):
+
+    # Load Weights
+    model.load_weights(weights_path)
+
+    for layer in model.layers:
+        print(layer.get_weights())
+
+if __name__ == "__main__":
+
+    # Load dataset as train and test sets
+    (x_train, y_train), (x_test, y_test) = mnist.load_data()
+
+    # # Set numeric type to float32 from uint8
+    # x_train = x_train.astype('uint8')
+    # x_test = x_test.astype('uint8')
+
+    # Normalize value to [0, 1]
+    # x_train /= 255
+    # x_test /= 255
+
+    # Transform lables to one-hot encoding
+    y_train = np_utils.to_categorical(y_train, 10)
+    y_test = np_utils.to_categorical(y_test, 10)
+
+    # Reshape the dataset into 4D array
+    x_train = x_train.reshape(x_train.shape[0], 28,28,1)
+    x_test = x_test.reshape(x_test.shape[0], 28,28,1)
+    
+    # Model initilization
+    model = LeNet()
+
+    # MNIST data fit (Comment this line if you already made the training)
+    Train(model, "LeNet_5.h5", x_train, y_train)
+
+    # Test accuracy
+    Test(model, "LeNet_5.h5", x_test, y_test)
+
+    # print float32/64 weights
+    print_weights(model, "LeNet_5.h5")
---
+++ LeNet.py
@@ -0,0 +1,215 @@
+import numpy as np
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torchvision.transforms as transforms
+from torch.utils.data import DataLoader
+from torch.utils.data.sampler import SubsetRandomSampler
+from torchvision import datasets
+from torchsummary import summary
+
+torch.set_default_dtype(torch.uint8)
+
+def data_processing(num_workers=0, batch_size=32, valid_sample=0.2):
+
+    transform = [transforms.Pad(2), transforms.ToTensor()]
+
+    # choose the training and test datasets
+    train_data = datasets.MNIST(root='data',
+                                train=True,
+                                download=True,
+                                transform=transforms.Compose(transform))
+    test_data = datasets.MNIST(root='data',
+                               train=False,
+                               download=True,
+                               transform=transforms.Compose(transform))
+
+    # Creating validation sampler
+    num_train = len(train_data)
+    indices = list(range(num_train))
+    np.random.shuffle(indices)
+    split = int(valid_sample * num_train)
+    train_idx, valid_idx = indices[split:], indices[:split]
+
+    # define sampler for batches
+    trainSampler = SubsetRandomSampler(train_idx)
+    validationSampler = SubsetRandomSampler(valid_idx)
+
+    # prepare data loaders
+    train_loader = DataLoader(train_data,
+                              batch_size=batch_size,
+                              sampler=trainSampler,
+                              num_workers=num_workers)
+    validation_loader = DataLoader(train_data,
+                                   batch_size=batch_size,
+                                   sampler=validationSampler,
+                                   num_workers=num_workers)
+    test_loader = DataLoader(test_data,
+                             batch_size=batch_size,
+                             num_workers=num_workers)
+
+    return train_loader, validation_loader, test_loader
+
+
+class LeNet(nn.Module):
+    def __init__(self):
+        super(LeNet, self).__init__()
+        self.conv1 = nn.Conv2d(1, 6, (5, 5)).uint8()
+        self.avg_pool1 = nn.AvgPool2d((2,2)).uint8()
+        self.conv2 = nn.Conv2d(6, 16, (5, 5)).uint8()
+        self.avg_pool2 = nn.AvgPool2d((2,2)).uint8()
+        self.conv3 = nn.Conv2d(16, 120, (5, 5)).uint8()
+        self.fc1 = nn.Linear(240, 84).uint8()
+        self.fc2 = nn.Linear(84, 10).uint8()
+
+    def forward(self, x):
+        x = torch.tanh(self.conv1(x))
+        x = self.avg_pool1(x)
+        x = torch.tanh(self.conv2(x))
+        x = self.avg_pool2(x)
+        x = torch.tanh(self.conv3(x))
+        x = torch.flatten(x)
+        x = F.tanh(self.fc1(x))
+        x = F.softmax(self.fc2(x), dim=-1)
+        return x
+
+
+def Train(model,
+          train_loader,
+          validation_loader,
+          optimizer,
+          criterion,
+          n_epochs=40):
+
+    # device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+
+    # model.to(device)
+
+    valid_loss_min = np.Inf
+
+    for epoch in range(n_epochs):
+
+        # monitor training loss
+        train_loss = 0.0
+        valid_loss = 0.0
+
+        ###################
+        # train the model #
+        ###################
+        model.train()  # prep model for training
+        for data, target in train_loader:
+            # data.to(device)
+            # target.to(device)
+            # clear the gradients of all optimized variables
+            optimizer.zero_grad()
+            # forward pass: compute predicted outputs by passing inputs to the model
+            output = model(data)
+            # calculate the loss
+            loss = criterion(output, target)
+            # backward pass: compute gradient of the loss with respect to model parameters
+            loss.backward()
+            # perform a single optimization step (parameter update)
+            optimizer.step()
+            # update running training loss
+            train_loss += loss.item() * data.size(0)
+
+        model.eval()
+        for data, target in validation_loader:
+            output = model(data)
+            loss = criterion(output, target)
+            valid_loss += loss.item() * data.size(0)
+
+        # print training statistics
+        # calculate average loss over an epoch
+        train_loss = train_loss / len(train_loader.sampler)
+        valid_loss = valid_loss / len(validation_loader.sampler)
+
+        print('Epoch: {} \tTraining Loss: {:.6f} \tValidation Loss: {:.6f}'.
+              format(epoch + 1, train_loss, valid_loss))
+        if valid_loss <= valid_loss_min:
+            print(
+                'Validation loss decreased ({:.6f} --> {:.6f}).  Saving model ...'
+                .format(valid_loss_min, valid_loss))
+            torch.save(model.state_dict(), 'model.pt')
+            valid_loss_min = valid_loss
+
+
+def Load(model, path="model.pt"):
+    return model.load_state_dict(torch.load(path))
+
+
+def Test(model, classes, test_loader):
+    # initialize lists to monitor test loss and accuracy
+    test_loss = 0.0
+    class_correct = list(0. for i in range(10))
+    class_total = list(0. for i in range(10))
+
+    model.eval()  # prep model for *evaluation*
+
+    for data, target in test_loader:
+        # forward pass: compute predicted outputs by passing inputs to the model
+        output = model(data)
+        # calculate the loss
+        loss = criterion(output, target)
+        # update test loss
+        test_loss += loss.item() * data.size(0)
+        # convert output probabilities to predicted class
+        _, pred = max(output, 1)
+        # compare predictions to true label
+        correct = np.squeeze(pred.eq(target.data.view_as(pred)))
+        # calculate test accuracy for each object class
+        for i in range(batch_size):
+            label = target.data[i]
+            class_correct[label] += correct[i].item()
+            class_total[label] += 1
+
+    # calculate and print avg test loss
+    test_loss = test_loss / len(test_loader.dataset)
+    print('Test Loss: {:.6f}\n'.format(test_loss))
+
+    for i in range(10):
+        if class_total[i] > 0:
+            print('Test Accuracy of %5s: %2d%% (%2d/%2d)' %
+                  (str(i), 100 * class_correct[i] / class_total[i],
+                   np.sum(class_correct[i]), np.sum(class_total[i])))
+        else:
+            print('Test Accuracy of %5s: N/A (no training examples)' %
+                  (classes[i]))
+
+    print('\nTest Accuracy (Overall): %2d%% (%2d/%2d)' %
+          (100. * np.sum(class_correct) / np.sum(class_total),
+           np.sum(class_correct), np.sum(class_total)))
+
+
+if __name__ == "__main__":
+
+    # number of subprocesses to use for data loading
+    num_workers = 0
+    # how many samples per batch to load
+    batch_size = 20
+    #  validation sample
+    valid_sample = 0.2
+
+    # initilize the model
+    model = LeNet()
+    summary(model.to("cuda"), (1, 32, 32))
+    # classes of MNIST
+    classes = list(range(10))
+
+    # specify loss function
+    criterion = nn.CrossEntropyLoss()
+    # specify optimizer
+    optimizer = torch.optim.SGD(model.parameters(), lr=0.01)
+
+    # 
+    train_loader, validation_loader, test_loader = data_processing(
+        num_workers, batch_size, valid_sample)
+
+    # Training the model on GPU if avialable
+    Train(model, train_loader, validation_loader, optimizer, criterion)
+
+    # Loading the model's weights
+    Load(model)
+
+    # Testing the model
+    Test(model, classes, test_loader)
